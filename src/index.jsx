ffde82d4d2bcc06bb467a9fe1fbd0a413e5a1f2a
Andrew Burns <andrewburns@MacBook-Pro.attlocal.net>
2020-07-09 14:29:47 -0400

    Update babel polyfills to support es6

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { App } from './App';
+import React from "react";
+import { render } from "react-dom";
+import { App } from "./App";
 
 function Root() {
-  return (
-  <App />
-  );
+  return <App />;
 }
 
-render(<Root />, document.getElementById('root'));
+render(<Root />, document.getElementById("root"));
--- src/jest.config.js
+++ src/jest.config.js
@@ -1,1 +1,6 @@
-import "@testing-library/jest-dom";
+import "@testing-library/jest-dom/extend-expect";
+import { render, fireEvent, wait } from "@testing-library/react";
+import "babel-polyfill";
+window.render = render;
+window.fireEvent = fireEvent;
+window.wait = wait;
