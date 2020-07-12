9c46c2e5e5d26b75be223a2eeb50dc9c5c025c38
Andrew Burns <andrewburns@MacBook-Pro.attlocal.net>
2020-07-12 09:58:47 -0400

    Update saga to call data transform

--- src/sagas/uploadLocationDataSaga.js
+++ src/sagas/uploadLocationDataSaga.js
@@ -1,14 +1,17 @@
-import { take, all, fork, put } from "redux-saga/effects";
+import { take, all, fork, put, call } from "redux-saga/effects";
 import types from "../reducers/actionTypes";
 import { fileUploadedSuccessfully } from "../reducers/actionCreators";
+import { fileTransformer } from "../services/fileTransformer";
 
 export function* pollForUpload() {
   while (true) {
     const action = yield take(types.SAVE_LOCATION_DATA);
     const { payload } = action;
-    const { fileInfo } = payload;
+    const { data, fileInfo } = payload;
 
     yield put(fileUploadedSuccessfully(fileInfo.name));
+
+    yield call(fileTransformer, data);
   }
 }
 
--- src/sagas/uploadLocationDataSaga.test.js
+++ src/sagas/uploadLocationDataSaga.test.js
@@ -3,10 +3,15 @@ import {
   fileUploadedSuccessfully,
 } from "../reducers/actionCreators";
 import { pollForUpload } from "./uploadLocationDataSaga";
-import { take, put } from "redux-saga/effects";
+import { take, put, call } from "redux-saga/effects";
 import types from "../reducers/actionTypes";
+import { fileTransformer } from "../services/fileTransformer";
 
 describe("upload location data saga", () => {
+  const uploadedData = {
+    payload: { fileInfo: { name: "aFile", data: "fileData" } },
+  };
+
   it("correctly listens when START FILE UPLOAD is dispatched", () => {
     const action = saveLocationData({
       data: "foo",
@@ -14,8 +19,11 @@ describe("upload location data saga", () => {
     const saga = pollForUpload(action);
 
     expect(saga.next().value).toEqual(take(types.SAVE_LOCATION_DATA));
-    expect(
-      saga.next({ payload: { fileInfo: { name: "aFile" } } }).value
-    ).toEqual(put(fileUploadedSuccessfully("aFile")));
+    expect(saga.next(uploadedData).value).toEqual(
+      put(fileUploadedSuccessfully("aFile"))
+    );
+    expect(saga.next(uploadedData).value).toEqual(
+      call(fileTransformer, undefined)
+    );
   });
 });
